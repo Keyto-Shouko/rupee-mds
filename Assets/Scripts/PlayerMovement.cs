82120f9ed603b266931ba9c1ebb4d4d67df039fc
Bruno AC <balvesca@gmail.com>
2023-10-04 15:13:59 +0200

    Added forest

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 _movement;
     private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+
     [Range(5f, 20f)]
     public float speed = 10f;
 
@@ -14,6 +16,7 @@ public class PlayerMovement : MonoBehaviour
     {
         Debug.Log("awake");
         _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
     // Start is called before the first frame update
     void Start()
@@ -27,6 +30,9 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
         _movement = new Vector2(horizontal, vertical).normalized;
+        _animator.SetFloat("Horizontal", horizontal);
+        _animator.SetFloat("Vertical", vertical);
+        _animator.SetFloat("Speed", _movement.sqrMagnitude);
         Debug.Log(_movement);
     }
 
