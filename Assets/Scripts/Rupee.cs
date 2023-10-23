0d3baab48ee8962e5628770574b7c69ae5fe424e
Bruno AC <balvesca@gmail.com>
2023-10-23 16:49:40 +0200

    Added particle color and light color

--- Assets/Scripts/Rupee.cs
+++ Assets/Scripts/Rupee.cs
@@ -9,6 +9,11 @@ public class Rupee : MonoBehaviour
     private SpriteRenderer _spriteRenderer;
     private AudioClip _collectedClip;
 
+    //add a light field to the rupee
+    public Light light;
+    //add a public particle system
+    public ParticleSystem particleSystem;
+
     public RupeeData Data{
         get => _data;
         set{
@@ -16,7 +21,11 @@ public class Rupee : MonoBehaviour
             score = _data.score;
             _spriteRenderer.color = _data.color;
             _collectedClip = _data.collectClip;
-            
+            //set the light color to the rupee color
+            light.color = _data.color;
+            //set the particle system color to the rupee color
+            var main = particleSystem.main;
+            main.startColor = _data.color;
         }
     }
     private void OnCollisionEnter2D(Collision2D other) {
